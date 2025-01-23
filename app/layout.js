239372ff95cb0bb578e86447ec0397895749df44
Wahiduz Zaman <156948092+Wahiduz89@users.noreply.github.com>
2025-01-23 20:37:59 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Outfit } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
-import { Provider } from './provider'
+
 
 
 
